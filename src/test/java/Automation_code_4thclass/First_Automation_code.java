f1ebd8f9f2643b27eb61d4340d7902d91d7472d3
Eyanakayes <aorinkayes@yahoo.com>
2023-02-01 21:07:46 -0500

    pushing first Automation code

---
+++ src/test/java/Automation_code_4thclass/First_Automation_code.java
@@ -0,0 +1,15 @@
+package Automation_code_4thclass;
+
+import org.openqa.selenium.WebDriver;
+
+public class First_Automation_code {
+	
+	public static WebDriver driver;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
